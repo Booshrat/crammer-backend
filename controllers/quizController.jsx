583983828acb94833792b58a29ab370cf69f7495
Stephan <Snj_Clarke133@outlook.com>
2023-12-18 16:58:17 +0000

    Quiz mongoDb connect

---
+++ controllers/quizController.jsx
@@ -0,0 +1,63 @@
+const Quiz = require('../models/quizModel');
+
+// Get all quizzes
+exports.getAllQuizzes = async (req, res) => {
+    try {
+        const quizzes = await Quiz.find();
+        res.json(quizzes);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+// Get a quiz by ID
+exports.getQuizById = async (req, res) => {
+    try {
+        const quizId = req.params.id;
+        const quiz = await Quiz.findById(quizId);
+        if (!quiz) {
+            return res.status(404).json({ error: 'Quiz not found' });
+        }
+        res.json(quiz);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+// Add a new quiz
+exports.createQuiz = async (req, res) => {
+    try {
+        const newQuiz = await Quiz.create(req.body);
+        res.status(201).json(newQuiz);
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid request' });
+    }
+};
+
+// Update a quiz by ID
+exports.updateQuizById = async (req, res) => {
+    try {
+        const quizId = req.params.id;
+        const updatedQuiz = await Quiz.findByIdAndUpdate(quizId, req.body, { new: true });
+        if (!updatedQuiz) {
+            return res.status(404).json({ error: 'Quiz not found' });
+        }
+        res.json(updatedQuiz);
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid request' });
+    }
+};
+
+// Delete a quiz by ID
+exports.deleteQuizById = async (req, res) => {
+    try {
+        const quizId = req.params.id;
+        const deletedQuiz = await Quiz.findByIdAndRemove(quizId);
+        if (!deletedQuiz) {
+            return res.status(404).json({ error: 'Quiz not found' });
+        }
+        res.json({ success: true, message: 'Quiz deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
--- server.js
+++ server.js
@@ -1,36 +1,66 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/userRoutes')
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
 
-const app = express()
-const port = 3000
-const URI = 'mongodb://localhost:27017'
+const app = express();
+const port = process.env.PORT || 3000;
+const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crammer-app';
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    console.log(req.path, req.method);
+    next();
+});
 
-app.use('/user', userRoutes)
+app.use('/user', userRoutes);
 
 app.get("/", (req, res) => {
     res.json({
         title: "Crammer App",
-        description: "Take part in the quizes and create your own flashcards"
+        description: "Take part in the quizzes and create your own flashcards"
     });
 });
 
-mongoose.connect(URI)
+app.get("/fetch-and-store-trivia", async (req, res) => {
+    try {
+        // Use the 'SECRET' variable in your code
+        const secret = process.env.SECRET;
+        console.log('Secret:', secret);
+
+        // Make a request to the Trivia API
+        const response = await axios.get('https://the-trivia-api.com/v2/questions/');
+        
+        // Assuming the response data is an array of trivia questions
+        const triviaQuestions = response.data;
+
+        // Assuming you have a Quiz model defined using Mongoose
+        const Quiz = require('./models/quizModel');
+
+        // Store questions in the MongoDB collection
+        await Quiz.insertMany(triviaQuestions);
+
+        res.json({ success: true, message: 'Trivia questions fetched and stored successfully.' });
+    } catch (error) {
+        console.error('Error fetching and storing trivia questions:', error);
+        res.status(500).json({ success: false, message: 'Internal server error.' });
+    }
+});
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
     .then(() => {
-        app.listen(port, () => {
-            console.log(`Connected to DB & Listening on port ${port}!`)
-        })
+      console.log('Connected to MongoDB!');
+      app.listen(port, () => {
+        console.log(`Listening on port ${port}!`);
+      });
     })
     .catch((error) => {
-        console.log(error)
-    })
-
+      console.error('Error connecting to MongoDB:', error);
+    });
